adfc37a78e0b79773cd942f38f4231d8f31fac11
Pushpit Upadhyay <coderpushpit@gmail.com>
2025-09-17 23:56:43 +0530

    asd

--- mental-health/tailwind.config.js
+++ mental-health/tailwind.config.js
@@ -1,37 +1,49 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        primary: '#585182',
-        primary: '#9CA3AF',
-        primary: '#4B5563',
-      },
-    },
-  },
-};
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+// module.exports = {
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: '#585182',
+//         primary: '#9CA3AF',
+//         primary: '#9CA3AF',
+//       },
+//     },
+//   },
+// };
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: '#585182',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#585182',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
